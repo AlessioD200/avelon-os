c2f6da139c630fd5bdc3b901cc6181bc016185a3
AlessioD <106388894+AlessioD200@users.noreply.github.com>
2026-01-16 20:14:21 +0100

    Start

---
+++ profiledef.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Avelon OS - Main Profile Definition
+
+iso_name="avelon-os"
+iso_label="AVELON_$(date +%Y%m)"
+iso_publisher="Avelon Team <https://github.com/jouwgebruikersnaam/avelon-os>"
+iso_application="Avelon OS Live/Rescue CD"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_Guide"]="0:0:755"
+)
